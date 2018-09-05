d68866300a97fd550f71bc75a6ac7da12e936503
Almir <almirpask@hotmail.com>
2018-09-04 21:43:43 -0300

    Transform User struct in a schema

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -1,7 +1,8 @@
 defmodule RumblWeb.UserController do 
     use RumblWeb, :controller
     alias Rumbl.Accounts
-    
+    alias Rumbl.Accounts.User
+
     def index(conn, _params) do
         users = Accounts.list_users()
         render(conn, "index.html", users: users)
