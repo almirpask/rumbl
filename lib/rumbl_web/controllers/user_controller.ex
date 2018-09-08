998955c9e976c1c06a9735c420d0857d1320232d
Almir <almirpask@hotmail.com>
2018-09-08 14:27:38 -0300

    fix required fields for user

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -23,7 +23,7 @@ defmodule RumblWeb.UserController do
             {:ok, user} ->
                 conn
                 |> put_flash(:info, "#{user.name} created!")
-                |> redirect(to: Routes.user_path(conn, :index))
+                |> redirect(to: user_path(conn, :index))
             {:error, %Ecto.Changeset{} = changeset} ->
                 render(conn, "new.html", changeset: changeset)
         end
