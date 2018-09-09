169375201d4a1fbc5870f50c56eed44c985bb751
Almir <almirpask@hotmail.com>
2018-09-09 02:43:38 -0300

    Create Login Form

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -4,6 +4,7 @@ defmodule Rumbl.Accounts do
     """
     alias Rumbl.Repo
     alias Rumbl.Accounts.User
+    import Ecto.Query
 
     def list_users do
         Repo.all(User)
@@ -130,5 +131,25 @@ defmodule Rumbl.Accounts do
   """
   def change_credential(%Credential{} = credential) do
     Credential.changeset(credential, %{})
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+      user = get_user_by_email(email)
+
+      cond do
+        user && Comeonin.Bcrypt.checkpw(given_pass, user.credential.password_hash) ->
+          {:ok, user}
+        user -> 
+          {:error, :unauthorized}
+        true -> 
+          Comeonin.Bcrypt.dummy_checkpw()
+          {:error, :not_found}
+      end
   end
 end
